949453df175cff668fc8312e37c75bc5a0011c30
Einzel <einzel0102@mail.ru>
2023-08-30 11:04:17 +0800

    "Простой калькулятор. Введи выражение через пробел"

---
+++ src/Isklyucheniya_v_programmirovanii/Seminar_3/CalculationException.java
@@ -0,0 +1,7 @@
+package Isklyucheniya_v_programmirovanii.Seminar_3;
+
+public class CalculationException extends Exception {
+    public CalculationException(String message) {
+        super(message);
+    }
+}
