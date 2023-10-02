cd8aead82ba03b678d04f28cddb9e07a3aa2b38a
Einzel <einzel0102@mail.ru>
2023-10-02 18:35:39 +0800

    "Практическая №4."

--- src/ArkhitekturaPO/Practical04/Main.java
+++ src/ArkhitekturaPO/Practical04/Main.java
@@ -2,27 +2,6 @@ package ArkhitekturaPO.Practical04;
 
 import java.util.Scanner;
 
-/**
- Проанализировав код, могу сказать следующее.
- Принципам SOLID, которым соответствует данный код:
-
- 1. Принцип единственной ответственности (Single Responsibility Principle) - каждый класс отвечает только за свою
- задачу: Main - за взаимодействие с пользователем, CSVContactWriter - за запись контактов в CSV-файл,
- CSVContactReader - за чтение контактов из CSV-файла.
-
- 2. Принцип открытости/закрытости (Open/Closed Principle) - классы CSVContactWriter и CSVContactReader
- закрыты для изменений, но открыты для расширения, т.е. их можно использовать для чтения/записи других форматов файлов.
-
- 3. Принцип подстановки Барбары Лисков (Liskov Substitution Principle) - интерфейсы ContactWriter и ContactReader
- могут быть реализованы любыми классами, которые умеют записывать/читать контакты из файлов.
-
- Принципам SOLID, которым не соответствует данный код:
-
- 1. Принцип инверсии зависимостей (Dependency Inversion Principle) - класс Main зависит от конкретных реализаций
- CSVContactWriter и CSVContactReader, а не от абстракций. Это может привести к проблемам при изменении
- или добавлении новых реализаций интерфейсов ContactWriter и ContactReader.
- */
-
 public class Main {
     public static void main(String[] args) {
         Scanner numberscanner = new Scanner(System.in);
